f6d113c7f3d109d4d809380bf1191ce51b861ff3
vigor13 <vigor13.dev@gmail.com>
2023-11-29 17:11:01 +0900

    chore(design-system-doc): add storybook react environment setting

--- docs/design-system-doc/.storybook/main.ts
+++ docs/design-system-doc/.storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from '@storybook/html-vite';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 import { join, dirname } from 'path';
 
@@ -6,18 +6,19 @@ import { join, dirname } from 'path';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
+// function getAbsolutePath(value: string): any {
+//   return dirname(require.resolve(join(value, 'package.json')));
+// }
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-interactions'),
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/html-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
--- docs/design-system-doc/src/header.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Header = (): React.ReactNode => {
-  return <nav>HEADER</nav>;
-};
