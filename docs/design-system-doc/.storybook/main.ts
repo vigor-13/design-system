1bebe6ee2f4130eb516096c1ba8dff8254e30ce7
vigor13 <vigor13.dev@gmail.com>
2023-12-03 16:41:55 +0900

    chore(design-system-doc): add `@react-ui/core` package to config

--- docs/design-system-doc/.storybook/main.ts
+++ docs/design-system-doc/.storybook/main.ts
@@ -1,7 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../node_modules/@react-ui/core/src/stories/*.mdx',
+    '../node_modules/@react-ui/core/src/stories/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
