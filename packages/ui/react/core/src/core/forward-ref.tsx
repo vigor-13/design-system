fc10c52cec11208d79e3700ab90d1640f16ba051
vigor13 <vigor13.dev@gmail.com>
2023-12-08 16:17:43 +0900

    feat(@react-ui/core): add forwardRef function

---
+++ packages/ui/react/core/src/core/forward-ref.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { type AnyObject } from '../utils';
+import {
+  type RightJoinProps,
+  type As,
+  type PropsOf,
+  type ComponentWithAs,
+} from './core.system.type';
+
+/**
+ * All credit goes to Chance (Reach UI), Haz (Reakit) and (fluentui)
+ * for creating the base type definitions upon which we improved on
+ */
+export function forwardRef<Props extends AnyObject, Component extends As>(
+  component: React.ForwardRefRenderFunction<
+    any,
+    RightJoinProps<PropsOf<Component>, Props> & { as?: As }
+  >,
+): ComponentWithAs<Component, Props> {
+  return React.forwardRef(component) as unknown as ComponentWithAs<
+    Component,
+    Props
+  >;
+}
--- packages/ui/react/core/src/core/index.ts
+++ packages/ui/react/core/src/core/index.ts
@@ -1,1 +1,2 @@
 export * from './core.factory';
+export * from './forward-ref';
