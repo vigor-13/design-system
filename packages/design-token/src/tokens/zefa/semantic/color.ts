fe0a824279356dfa0d9d1cb678cb5f677bbd8e7f
vigor13 <vigor13.dev@gmail.com>
2024-02-17 22:17:23 +0900

    feat(design-token): add zefa color token

---
+++ packages/design-token/src/tokens/zefa/semantic/color.ts
@@ -0,0 +1,170 @@
+export const zefa = {
+  semantic: {
+    primary: {
+      default: {
+        value: '{zefa.static.color.purple.120}',
+        description: 'Primary',
+      },
+      hover: {
+        value: '{zefa.static.color.purple.140}',
+        description: 'Primary Hover',
+      },
+      press: {
+        value: '{zefa.static.color.purple.160}',
+        description: 'Primary Press',
+      },
+      background: {
+        default: {
+          value: '{zefa.static.color.purple.120}',
+          description: 'Primary Background',
+          lighten: 5,
+        },
+        light: {
+          value: '{zefa.static.color.purple.120}',
+          description: 'Primary Background Light',
+          lighten: 10,
+        },
+      },
+    },
+    content: {
+      default: {
+        value: '#232323',
+        description: 'Content',
+      },
+      medium: {
+        value: '{zefa.static.color.gray.140}',
+        description: 'Content Medium',
+      },
+      light: {
+        value: '{zefa.static.color.gray.100}',
+        description: 'Content Light',
+      },
+      disabled: {
+        value: '{zefa.static.color.gray.60}',
+        description: 'Content Disabled',
+      },
+    },
+    warning: {
+      default: {
+        value: '{zefa.static.color.yellow.120}',
+        description: 'Warning',
+      },
+      hover: {
+        value: '{zefa.static.color.yellow.140}',
+        description: 'Warning Hover',
+      },
+      press: {
+        value: '{zefa.static.color.yellow.160}',
+        description: 'Warning Press',
+      },
+      background: {
+        default: {
+          value: '#DCBA00',
+          description: 'Warning Background',
+          ligten: 5,
+        },
+        light: {
+          value: '#DCBA00',
+          description: 'Warning Background Light',
+          lighten: 10,
+        },
+      },
+    },
+    success: {
+      default: {
+        value: '{zefa.static.color.green.120}',
+        description: 'Success',
+      },
+      hover: {
+        value: '{zefa.static.color.green.140}',
+        description: 'Success Hover',
+      },
+      press: {
+        value: '{zefa.static.color.green.160}',
+        description: 'Success Press',
+      },
+      background: {
+        default: {
+          value: '#01882F',
+          description: 'Success Background',
+          lighten: 5,
+        },
+        light: {
+          value: '#01882F',
+          description: 'Success Background Light',
+          lighten: 10,
+        },
+      },
+    },
+    error: {
+      default: {
+        value: '{zefa.static.color.red.120}',
+        description: 'Error',
+      },
+      hover: {
+        value: '{zefa.static.color.red.140}',
+        description: 'Error Hover',
+      },
+      press: {
+        value: '{zefa.static.color.red.160}',
+        description: 'Error Press',
+      },
+      background: {
+        default: {
+          value: '#E00001',
+          description: 'Error Background',
+          lighten: 5,
+        },
+        light: {
+          value: '#E00001',
+          description: 'Error Background Light',
+          lighten: 10,
+        },
+      },
+    },
+    neutral: {
+      default: {
+        value: '{zefa.static.color.gray.120}',
+        description: 'Neutral',
+      },
+      hover: {
+        value: '{zefa.static.color.gray.140}',
+        description: 'Neutral Hover',
+      },
+      press: {
+        value: '{zefa.static.color.gray.160}',
+        description: 'Neutral Press',
+      },
+      background: {
+        default: {
+          value: '{zefa.static.color.gray.120}',
+          description: 'Neutral Background',
+          lighten: 5,
+        },
+        light: {
+          value: '{zefa.static.color.gray.120}',
+          description: 'Neutral Background Light',
+          lighten: 10,
+        },
+      },
+    },
+    background: {
+      default: {
+        value: '#F6F6F6',
+        description: 'Background',
+      },
+      disabled: {
+        value: '{zefa.static.color.gray.0}',
+        description: 'Background Disabled',
+      },
+      full: {
+        value: '{zefa.static.color.white}',
+        description: 'Background Full',
+      },
+    },
+    stroke: {
+      value: '{zefa.static.color.gray.20}',
+      description: 'Stroke',
+    },
+  },
+};
---
+++ packages/design-token/src/tokens/zefa/static/color.ts
@@ -0,0 +1,290 @@
+export const zefa = {
+  static: {
+    color: {
+      white: {
+        value: '#FFFFFF',
+        description: 'White',
+      },
+      black: {
+        value: '#000000',
+        description: 'Black',
+      },
+      purple: {
+        0: {
+          value: '#D4B1F4',
+          description: 'Purple 0',
+        },
+        20: {
+          value: '#C89BF1',
+          description: 'Purple 20',
+        },
+        40: {
+          value: '#BB84ED',
+          description: 'Purple 40',
+        },
+        60: {
+          value: '#AE6DEB',
+          description: 'Purple 60',
+        },
+        80: {
+          value: '#9C4DE5',
+          description: 'Purple 80',
+        },
+        100: {
+          value: '#9641E5',
+          description: 'Purple 100',
+        },
+        120: {
+          value: '#8A2BE2',
+          description: 'Purple 120',
+        },
+        140: {
+          value: '#6C1EC4',
+          description: 'Purple 140',
+        },
+        160: {
+          value: '#4E12A8',
+          description: 'Purple 160',
+        },
+        180: {
+          value: '#3A0D90',
+          description: 'Purple 180',
+        },
+        200: {
+          value: '#280978',
+          description: 'Purple 200',
+        },
+      },
+      cyan: {
+        0: {
+          value: '#99E9DD',
+          description: 'Cyan 0',
+        },
+        20: {
+          value: '#7EE4D4',
+          description: 'Cyan 20',
+        },
+        40: {
+          value: '#66DECC',
+          description: 'Cyan 40',
+        },
+        60: {
+          value: '#4BD9C3',
+          description: 'Cyan 60',
+        },
+        80: {
+          value: '#32D3BA',
+          description: 'Cyan 80',
+        },
+        100: {
+          value: '#19CEB2',
+          description: 'Cyan 100',
+        },
+        120: {
+          value: '#00C8AA',
+          description: 'Cyan 120',
+        },
+        140: {
+          value: '#03B395',
+          description: 'Cyan 140',
+        },
+        160: {
+          value: '#019D81',
+          description: 'Cyan 160',
+        },
+        180: {
+          value: '#03876D',
+          description: 'Cyan 180',
+        },
+        200: {
+          value: '#02735A',
+          description: 'Cyan 200',
+        },
+      },
+      yellow: {
+        0: {
+          value: '#F1E399',
+          description: 'Yellow 0',
+        },
+        20: {
+          value: '#EDDC7F',
+          description: 'Yellow 20',
+        },
+        40: {
+          value: '#E9D566',
+          description: 'Yellow 40',
+        },
+        60: {
+          value: '#E6CE4C',
+          description: 'Yellow 60',
+        },
+        80: {
+          value: '#E4C733',
+          description: 'Yellow 80',
+        },
+        100: {
+          value: '#E5C51A',
+          description: 'Yellow 100',
+        },
+        120: {
+          value: '#DCBA01',
+          description: 'Yellow 120',
+        },
+        140: {
+          value: '#C59E00',
+          description: 'Yellow 140',
+        },
+        160: {
+          value: '#AE8400',
+          description: 'Yellow 160',
+        },
+        180: {
+          value: '#976C02',
+          description: 'Yellow 180',
+        },
+        200: {
+          value: '#835405',
+          description: 'Yellow 200',
+        },
+      },
+      green: {
+        0: {
+          value: '#99CFAB',
+          description: 'Green 0',
+        },
+        20: {
+          value: '#80C397',
+          description: 'Green 20',
+        },
+        40: {
+          value: '#67B680',
+          description: 'Green 40',
+        },
+        60: {
+          value: '#4DAB6C',
+          description: 'Green 60',
+        },
+        80: {
+          value: '#339F57',
+          description: 'Green 80',
+        },
+        100: {
+          value: '#199342',
+          description: 'Green 100',
+        },
+        120: {
+          value: '#01882E',
+          description: 'Green 120',
+        },
+        140: {
+          value: '#007322',
+          description: 'Green 140',
+        },
+        160: {
+          value: '#005F17',
+          description: 'Green 160',
+        },
+        180: {
+          value: '#034D14',
+          description: 'Green 180',
+        },
+        200: {
+          value: '#053B0F',
+          description: 'Green 200',
+        },
+      },
+      red: {
+        0: {
+          value: '#F79999',
+          description: 'Red 0',
+        },
+        20: {
+          value: '#F57F80',
+          description: 'Red 20',
+        },
+        40: {
+          value: '#F36666',
+          description: 'Red 40',
+        },
+        60: {
+          value: '#F14B4D',
+          description: 'Red 60',
+        },
+        80: {
+          value: '#EF3332',
+          description: 'Red 80',
+        },
+        100: {
+          value: '#E81113',
+          description: 'Red 100',
+        },
+        120: {
+          value: '#DF0001',
+          description: 'Red 120',
+        },
+        140: {
+          value: '#C50103',
+          description: 'Red 140',
+        },
+        160: {
+          value: '#B70004',
+          description: 'Red 160',
+        },
+        180: {
+          value: '#9F0105',
+          description: 'Red 180',
+        },
+        200: {
+          value: '#860105',
+          description: 'Red 200',
+        },
+      },
+      gray: {
+        0: {
+          value: '#F1F1F1',
+          description: 'Gray 0',
+        },
+        20: {
+          value: '#E8E8E8',
+          description: 'Gray 20',
+        },
+        40: {
+          value: '#D0D0D0',
+          description: 'Gray 40',
+        },
+        60: {
+          value: '#B9B9B9',
+          description: 'Gray 60',
+        },
+        80: {
+          value: '#A2A2A2',
+          description: 'Gray 80',
+        },
+        100: {
+          value: '#767676',
+          description: 'Gray 100',
+        },
+        120: {
+          value: '#595959',
+          description: 'Gray 120',
+        },
+        140: {
+          value: '#4F4F4F',
+          description: 'Gray 140',
+        },
+        160: {
+          value: '#404040',
+          description: 'Gray 160',
+        },
+        180: {
+          value: '#2E2E2E',
+          description: 'Gray 180',
+        },
+        200: {
+          value: '#1F1D22',
+          description: 'Gray 200',
+        },
+      },
+    },
+  },
+};
